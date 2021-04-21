015fecadfc4eac0f3670d1087e20c099c1c9ad0d
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2021-04-21 18:20:41 -0500

    rename

---
+++ boostedzprime/ddt_processor.py
@@ -0,0 +1,163 @@
+import logging
+import numpy as np
+from coffea import processor, hist
+#from boostedhiggs.twoProngGRU import *
+from coffea.nanoaod.methods import collection_methods, Candidate
+collection_methods["FatJetPFCands"] = Candidate
+
+from boostedhiggs.corrections import (
+    corrected_msoftdrop,
+    add_pileup_weight,
+    add_jetTriggerWeight,
+)
+
+class DDTProcessor(processor.ProcessorABC):
+    def __init__(self, year='2017'):
+        self._year = year
+        self._triggers = {
+            '2017': [
+                'PFHT1050',
+                'AK8PFJet400_TrimMass30',
+                'AK8PFJet420_TrimMass30',
+                'AK8PFHT800_TrimMass50',
+                'PFJet500',
+                'AK8PFJet500',
+                'AK8PFJet550',
+                'CaloJet500_NoJetID',
+                'CaloJet550_NoJetID', 
+                 ]
+        }
+        self._accumulator = processor.dict_accumulator({
+            'sumw': processor.defaultdict_accumulator(float),
+            'jet_kin': hist.Hist(
+                'Events',
+                hist.Cat('dataset', 'Dataset'),
+                hist.Cat('region', 'Region'),
+                hist.Bin('jet_pt', r'Jet $p_{T}$ [GeV]', 100, 200, 1200),
+                hist.Bin('jet_rho', r'Jet $\rho$', 180, -5.5, -2.),
+                #hist.Bin('jet_mass', r'Jet $m_{SD}$', 41, 40, 350),
+                hist.Bin('jet_in_v3', 'IN  value', 100, 0.0, 1.0),
+                #hist.Bin('jet_twoProngGru', r'Jet GRU score', 100, 0., 1.0),
+                #hist.Bin('jet_n2b1', r'Jet N_{2}}score', 100, 0., 0.5),
+            ),
+            'cutflow': hist.Hist(
+                'Events',
+                hist.Cat('dataset', 'Dataset'),
+                hist.Cat('region', 'Region'),
+                hist.Bin('cut', 'Cut index', 11, 0, 11),
+            ),
+        })
+
+    @property
+    def accumulator(self):
+        return self._accumulator
+
+    def process(self, events):
+        if(len(events) == 0): return output
+
+        dataset = events.metadata['dataset']
+        isRealData = False
+        selection = processor.PackedSelection()
+        weights = processor.Weights(len(events))
+        output = self.accumulator.identity()
+        output['sumw'][dataset] += events.genWeight.sum()
+
+        trigger_fatjet = np.zeros(events.size, dtype='bool')
+
+        # trigger paths
+        for t in self._triggers[self._year]:
+            trigger_fatjet = trigger_fatjet | events.HLT[t]
+        selection.add('fatjet_trigger', trigger_fatjet)
+
+        # run model on PFCands associated to FatJet (FatJetPFCands)
+        #events.FatJet.array.content["PFCands"] = type(events.FatJetPFCands.array).fromcounts(events.FatJet.nPFConstituents.flatten(), events.FatJetPFCands.flatten())
+        #events.FatJet.array.content["twoProngGru"] = run_model(events.FatJet.flatten())
+
+        fatjets = events.FatJet
+        IN  = events.IN
+
+        fatjets['msdcorr'] = corrected_msoftdrop(fatjets)
+        fatjets['rhocorr'] = 2*np.log(fatjets.msdcorr/fatjets.pt)
+        fatjets['in_v3'] = IN.v3
+        candidatejet = fatjets[
+            # https://github.com/DAZSLE/BaconAnalyzer/blob/master/Analyzer/src/VJetLoader.cc#L269
+            (fatjets.pt > 200)
+            & (abs(fatjets.eta) < 2.5)
+            # & fatjets.isLoose  # not always available
+            & (fatjets.rhocorr >= -5.5)
+            & (fatjets.rhocorr <= -2)
+        ][:, 0:1]
+
+        #events.FatJet = candidatejet
+
+
+        # basic jet selection
+        selection.add('minjetkin', (
+            (candidatejet.pt >= 200)
+            & (candidatejet.msdcorr >= 40.)
+            & (abs(candidatejet.eta) < 2.5)
+        ).any())
+
+        # lep veto
+        nmuons = (
+            (events.Muon.pt > 10)
+            & (abs(events.Muon.eta) < 2.4)
+            & (events.Muon.pfRelIso04_all < 0.25)
+            & (events.Muon.looseId).astype(bool)
+        ).sum()
+
+        nelectrons = (
+            (events.Electron.pt > 10)
+            & (abs(events.Electron.eta) < 2.5)
+            & (events.Electron.cutBased >= events.Electron.LOOSE)
+        ).sum()
+
+        ntaus = (
+            (events.Tau.pt > 20)
+            & (events.Tau.idDecayMode).astype(bool)
+            # bacon iso looser than Nano selection
+        ).sum()
+        selection.add('jetid', candidatejet.isTight.any())
+
+        selection.add('noleptons', (nmuons == 0) & (nelectrons == 0) & (ntaus == 0))
+        #weights.add('genweight', events.genWeight)
+        #add_pileup_weight(weights, events.Pileup.nPU, self._year, dataset)
+        #add_jetTriggerWeight(weights, candidatejet.msdcorr, candidatejet.pt, self._year)
+
+        regions = {
+           'signal' : ['fatjet_trigger', 'minjetkin','noleptons','jetid']
+        }
+        for region, cuts in regions.items():
+            allcuts = set()
+            output['cutflow'].fill(dataset=dataset, region=region, cut=0)
+            for i, cut in enumerate(cuts):
+                allcuts.add(cut)
+                cut = selection.all(*allcuts)
+                output['cutflow'].fill(dataset=dataset, region=region, cut=i + 1)# weight=weights.weight()[cut])
+
+        def normalize(val, cut):
+            return val[cut].pad(1, clip=True).fillna(0).flatten()
+
+        def fill(region, systematic=None, wmod=None):
+            selections = regions[region]
+            cut = selection.all(*selections)
+            sname = 'nominal' if systematic is None else systematic
+            weight = weights.weight()[cut]
+            output['jet_kin'].fill(
+                dataset=dataset,
+                region=region,
+                jet_pt=normalize(candidatejet.pt, cut),
+                #jet_mass=normalize(candidatejet.msdcorr, cut),
+                jet_rho=normalize(2*np.log(candidatejet.msdcorr/candidatejet.pt), cut),
+                jet_in_v3=normalize(candidatejet.in_v3, cut),
+                #jet_n2b1=normalize(candidatejet.n2b1, cut),
+            )
+
+        for region in regions:
+            fill(region)
+
+        return output
+
+    def postprocess(self, accumulator):
+        return accumulator
+
